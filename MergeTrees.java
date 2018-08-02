7769801dba29bfbcee952e8549966f86117127db
Manoj Kumar Shivaswami <manojkumarsmks@gmail.com>
2018-08-02 14:07:30 -0600

    Merge Trees

--- MergeTrees.java
+++ MergeTrees.java
@@ -1,27 +1,27 @@
 // https://leetcode.com/problems/merge-two-binary-trees/description/
-public class MergeTrees extends Tree{
+public class MergeTrees extends Tree {
 
 	public static void main(String[] args) {
 		MergeTrees mergeTrees = new MergeTrees();
-		mergeTrees.Merge(mergeTrees.Example1(), mergeTrees.Example2());
-		
+		mergeTrees.InOrderRecursive(mergeTrees.Merge(mergeTrees.Example1(), mergeTrees.Example2()));
+
 	}
-	
-	public void Merge(Node n1, Node n2) {
-		System.out.println("N1 "+n1.value + " N2 "+n2.value);
-		if(n1 == null && n2 == null)
-			return;
-		
-		if(n1 != null && n2 != null) {
+
+	public Node Merge(Node n1, Node n2) {
+
+		if (n1 == null && n2 == null)
+			return null;
+
+		if (n1 != null && n2 != null) {
 			n1.value += n2.value;
+		} else {
+			return n1 == null ? n2 : n1;
+
 		}
-		
-		if(n1.left == null && n2.left != null) {
-			n1.left = new Node(n2.left.value);
-		}
-		Merge(n1.left, n2.left);
-		Merge(n1.right, n2.right);
-		
+
+		n1.left = Merge(n1.left, n2.left);
+		n1.right = Merge(n1.right, n2.right);
+		return n1;
 	}
 
 }
